87b4bd33fcbe73ab052fa4fe5677daa43405c382
omkar shelke <shelkeom230@gmail.com>
2025-08-17 12:36:09 +0530

    strings completed, started with linked list

---
+++ linked list/basics.py
@@ -0,0 +1,189 @@
+# basics of linked list
+import sys, threading
+
+
+# creating a linked list Node
+class Node:
+    def __init__(self, data1, next1=None):
+        self.data = data1
+        self.next = next1
+
+
+# traverse the LL
+def printLL(head):
+    while head is not None:
+        print(head.data, end=" ")
+        head = head.next
+
+
+# find length of LL
+def lengthLL(head):
+    cnt = 0
+    while head is not None:
+        cnt += 1
+        head = head.next
+    return cnt
+
+
+# insert a new node at the head
+def insertNodeLL(head, val):
+    newNode = Node(val, head)
+    return newNode
+
+
+# search a node
+def searchNode(head, val):
+    while head is not None:
+        if head.data == val:
+            return "present"
+        head = head.next
+    return "not present"
+
+
+# delete head of LL
+def deleteHeadLL(head):
+    head = head.next
+    return head
+
+
+# delete tail of a LL
+def deleteTailLL(head):
+    if head == None or head.next == None:
+        return None
+
+    temp = head
+    while temp.next.next is not None:
+        temp = temp.next
+    temp.next = None
+    return head
+
+
+# delete kth node of a linked list
+def deleteKthNodeLL(head, k):
+    if head == None:
+        return head
+
+    if k == 1:
+        head = head.next
+        return head
+
+    cnt = 0
+    temp = head
+    prev = None
+
+    while temp is not None:
+        cnt += 1
+
+        if cnt == k:
+            prev.next = prev.next.next
+            break
+        prev = temp
+        temp = temp.next
+    return head
+
+
+# delete  node of a linked list by value
+def deleteNodeByValLL(head, ele):
+    if head == None:
+        return head
+
+    if head.data == ele:
+        head = head.next
+        return head
+
+    temp = head
+    prev = None
+
+    while temp is not None:
+
+        if temp.data == ele:
+            prev.next = prev.next.next
+            break
+        prev = temp
+        temp = temp.next
+    return head
+
+
+# insert a node at start of LL
+def insertAtHeadLL(head, val):
+    newNode = Node(val, head)
+    head = newNode
+    return head
+
+
+# insert a node at end of LL
+def insertAtTailLL(head, val):
+    if head == None:
+        return Node(val, None)
+
+    temp = head
+    while temp.next is not None:
+        temp = temp.next
+
+    newNode = Node(val, None)
+    temp.next = newNode
+    return head
+
+
+# insert at given position k
+def insertAtPosKLL(head, val, k):
+    if head == None:
+        if k == 1:
+            return Node(val, head)
+        else:
+            # invalid k value when head is null
+            return None
+
+    # k >1
+    # find the previous node
+    cnt = 0
+    temp = head
+    while temp != None:
+        cnt += 1
+        if cnt == k - 1:
+            break
+        temp = temp.next
+    newNode = Node(val, None)
+    newNode.next = temp.next
+    temp.next = newNode
+    return head
+
+
+# insert before given value x
+def insertBeforeXLL(head, val, x):
+    if head == None:
+        return head
+
+    if head.data == x:
+        return Node(val, head)
+
+    temp = head
+
+    while temp.next.data != x:
+        temp = temp.next
+    newNode = Node(val, None)
+    newNode.next = temp.next
+    temp.next = newNode
+    return head
+
+
+def main():
+    sys.stdin = open("linked list/input.txt", "r")
+    sys.stdout = open("linked list/output.txt", "w")
+    input = sys.stdin.readline
+
+    # start here
+    # s,t = list(map(str,input().split()))
+    # string = input()
+
+    # create a node with value 2
+    head = Node(2)
+    head.next = Node(3)
+    head.next.next = Node(5)
+    head.next.next.next = Node(6)
+
+    head = insertBeforeXLL(head, 100, 3)
+    printLL(head)
+
+
+threading.Thread(target=main).start()
--- strings/medium.py
+++ strings/medium.py
@@ -1,239 +1,252 @@
-# strings medium problems 
+# strings medium problems
 import sys, threading
-from collections import Counter,defaultdict
+from collections import Counter, defaultdict
+
 
 # sort string by freq of chars - 0(klogk)
 def sortString(string):
-    freq={}
+    freq = {}
     for char in string:
-        freq[char]=freq.get(char,0)+1
+        freq[char] = freq.get(char, 0) + 1
 
-    # sort the dict by values desc 
-    sorted_list=sorted(freq.items(),key=lambda item: item[1], reverse=True)
-    
-    # append char in the resuult 
-    res=[]
-    for char,count in sorted_list:
-        if char!=' ':
-            res.append(char*count)
-    return ''.join(res) 
+    # sort the dict by values desc
+    sorted_list = sorted(freq.items(), key=lambda item: item[1], reverse=True)
+
+    # append char in the resuult
+    res = []
+    for char, count in sorted_list:
+        if char != " ":
+            res.append(char * count)
+    return "".join(res)
+
 
 # sort string by freq of chars - better
 def sortStringBetter(string):
-    count=Counter(string) # char-> cnt 
-    buckets=defaultdict(list) # cnt-> [char]
+    count = Counter(string)  # char-> cnt
+    buckets = defaultdict(list)  # cnt-> [char]
 
-    # fill the buckets dict 
-    for char,cnt in count.items():
+    # fill the buckets dict
+    for char, cnt in count.items():
         buckets[cnt].append(char)
-    
-    res=[]
-    for i in range(len(string),0,-1):
+
+    res = []
+    for i in range(len(string), 0, -1):
         for char in buckets[i]:
-            if char!=' ':
-                    
-                res.append(char*i)
-    
+            if char != " ":
+
+                res.append(char * i)
+
     return "".join(res)
 
-# maximum nesting depth of parentheses -- using stack 
+
+# maximum nesting depth of parentheses -- using stack
 def maxDepthStack(string):
-    stack=[]
-    maxDepth=0 
+    stack = []
+    maxDepth = 0
 
     for char in string:
-        if char=='(':
+        if char == "(":
             stack.append(char)
-            maxDepth=max(maxDepth,len(stack))
-        elif char==')':
+            maxDepth = max(maxDepth, len(stack))
+        elif char == ")":
             if stack:
                 stack.pop()
-    return maxDepth   
+    return maxDepth
 
-# maximum nesting depth of parentheses -- without stack 
+
+# maximum nesting depth of parentheses -- without stack
 def maxDepthWithoutStack(string):
-    depth=0
-    maxDepth=0
+    depth = 0
+    maxDepth = 0
     for char in string:
-        if char=='(':
-            depth+=1
-            maxDepth=max(maxDepth,depth)
-        elif char==')':
-            depth-=1
-    return maxDepth 
+        if char == "(":
+            depth += 1
+            maxDepth = max(maxDepth, depth)
+        elif char == ")":
+            depth -= 1
+    return maxDepth
 
-# roman to integer conversion -- optimal  
+
+# roman to integer conversion -- optimal
 def romanToIntConv(romanstr):
-    value = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-    }
+    value = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-    integer=0
-    n=len(romanstr)
+    integer = 0
+    n = len(romanstr)
     for i in range(n):
-        if i<n-1 and value[romanstr[i]]<value[romanstr[i]]:
-            integer-=value[romanstr[i]]
+        if i < n - 1 and value[romanstr[i]] < value[romanstr[i]]:
+            integer -= value[romanstr[i]]
         else:
-            integer+=value[romanstr[i]]
-    return integer 
+            integer += value[romanstr[i]]
+    return integer
+
 
 # implement atoi (string to integer)
 def atoi(string):
-    # remove left whitespaces 
-    string=string.lstrip()
+    # remove left whitespaces
+    string = string.lstrip()
 
     if not string:
         return 0
     # determine the sign and start from the first digit
-    sign=1
-    i=0
+    sign = 1
+    i = 0
 
-    if string[0]=='-':
-        sign-=2
-        i+=1
-    if string[0]=='+':
-        i+=1
-    
-    num=0
-    # store the result until we find a non-digit 
-    while i<len(string) and string[i].isdigit():
-        num=num*10+int(string[i])
-        i+=1
-    
-    # add the sign 
-    num*=sign 
+    if string[0] == "-":
+        sign -= 2
+        i += 1
+    if string[0] == "+":
+        i += 1
 
-    # handle the range 
-    INT_MIN=-2**31 
-    INT_MAX=+2**31-1 
+    num = 0
+    # store the result until we find a non-digit
+    while i < len(string) and string[i].isdigit():
+        num = num * 10 + int(string[i])
+        i += 1
 
-    if num<INT_MIN:
-        return INT_MIN 
-    elif num>INT_MAX:
-        return INT_MAX 
-    return num 
+    # add the sign
+    num *= sign
+
+    # handle the range
+    INT_MIN = -(2**31)
+    INT_MAX = +(2**31) - 1
+
+    if num < INT_MIN:
+        return INT_MIN
+    elif num > INT_MAX:
+        return INT_MAX
+    return num
+
 
 # count number of substrings -- brute force
 def cntSubstrings(string):
-    n=len(string)
+    n = len(string)
 
-    cnt=0
+    cnt = 0
     for i in range(n):
-        for j in range(i+2,n):
-            substr=string[i:j+1]
-            if 'a' in substr and 'b' in substr and 'c' in substr: 
-                cnt+=1
-    
-    return cnt 
+        for j in range(i + 2, n):
+            substr = string[i : j + 1]
+            if "a" in substr and "b" in substr and "c" in substr:
+                cnt += 1
 
-# count number of substrings -- optimal 1 
+    return cnt
+
+
+# count number of substrings -- optimal 1
 def cntSubstrOptimal(string):
-    n=len(string)
-    count={'a':0,'b':0,'c':0}
-    ans=0
-    left=0
+    n = len(string)
+    count = {"a": 0, "b": 0, "c": 0}
+    ans = 0
+    left = 0
 
     for right in range(n):
-        count[string[right]]+=1
+        count[string[right]] += 1
 
-        while count['a']>0 and count['b']>0 and count['c']>0:
-            ans+=(n-right)
-            count[string[left]]-=1
-            left+=1
-    return ans  
+        while count["a"] > 0 and count["b"] > 0 and count["c"] > 0:
+            ans += n - right
+            count[string[left]] -= 1
+            left += 1
+    return ans
 
-# count number of substrings -- optimal 2 
+
+# count number of substrings -- optimal 2
 def cntSubstringsOptimal(string):
-    n=len(string)
-    lastSeen=[-1,-1,-1]
-    cnt=0
+    n = len(string)
+    lastSeen = [-1, -1, -1]
+    cnt = 0
 
     for i in range(n):
-        lastSeen[ord(string[i])-ord('a')]=i 
+        lastSeen[ord(string[i]) - ord("a")] = i
 
-        if lastSeen[0]!=-1 and lastSeen[1]!=-1 and lastSeen[2]!=-1:
+        if lastSeen[0] != -1 and lastSeen[1] != -1 and lastSeen[2] != -1:
 
-            cnt+=(1+min(lastSeen[0],lastSeen[1],lastSeen[2]))
-    return cnt 
+            cnt += 1 + min(lastSeen[0], lastSeen[1], lastSeen[2])
+    return cnt
 
-# longest palindromic substring -- brute 
+
+# longest palindromic substring -- brute
 def longPalindromicSubstrBrute(string):
-    n=len(string)
+    n = len(string)
 
-    maxLength=0
-    best_substr="" 
+    maxLength = 0
+    best_substr = ""
     for i in range(n):
-        for j in range(i,n):
-            substr=string[i:j+1]
-            if substr==substr[::-1]:
-                if len(substr)>maxLength:
-                    maxLength=len(substr)
-                    best_substr=substr 
-    return best_substr 
+        for j in range(i, n):
+            substr = string[i : j + 1]
+            if substr == substr[::-1]:
+                if len(substr) > maxLength:
+                    maxLength = len(substr)
+                    best_substr = substr
+    return best_substr
 
-# longest palindromic substring -- optimal 
+
+# longest palindromic substring -- optimal
 def longestPalindromicOptimal(string):
-    n=len(string)
-    resLen=0
-    res="" 
+    n = len(string)
+    resLen = 0
+    res = ""
 
     for i in range(n):
-        left,right=i,i 
+        left, right = i, i
 
-        # odd length 
-        while left>=0 and right<n and string[left]==string[right]:
-            if(right-left+1)>resLen:
-                res=string[left:right+1]
-                resLen=right-left+1
-            left-=1
-            right+=1
-        
-        # even length 
-        left,right=i,i+1 
-        while left>=0 and right<n and string[left]==string[right]:
-            if(right-left+1)>resLen:
-                res=string[left:right+1]
-                resLen=right-left+1
-            left-=1
-            right+=1
-    return res 
+        # odd length
+        while left >= 0 and right < n and string[left] == string[right]:
+            if (right - left + 1) > resLen:
+                res = string[left : right + 1]
+                resLen = right - left + 1
+            left -= 1
+            right += 1
 
-# sum of beauty of all substrings -- brute 
+        # even length
+        left, right = i, i + 1
+        while left >= 0 and right < n and string[left] == string[right]:
+            if (right - left + 1) > resLen:
+                res = string[left : right + 1]
+                resLen = right - left + 1
+            left -= 1
+            right += 1
+    return res
+
+
+# sum of beauty of all substrings -- brute
 def sumBeautyBrute(string):
-    n=len(string)
+    n = len(string)
 
-    sumVal=0
+    sumVal = 0
 
     for i in range(n):
-        freq={}
-        for j in range(i,n):
-            for k in range(i,j+1):
-                freq[string[k]]=freq.get(string[k],0)+1
-            
-            minFreq,maxFreq=0,0
+        freq = defaultdict(int)
+        for j in range(i, n):
+            freq[string[j]] += 1
 
-            for val in freq.values():
-                minFreq=min(minFreq,val)
-                maxFreq=max(maxFreq,val)
-            
-            if maxFreq-maxFreq!=0:
-                sumVal+=(maxFreq-minFreq)
-    return sumVal 
+            minFreq = min(freq.values())
+            maxFreq = max(freq.values())
+            sumVal += maxFreq - minFreq
+    return sumVal
+
+
+# reverse words in a string
+def revWordsString(string):
+    # remove whitespaces
+    string = string.strip()
+    # get list of words
+    words = string.split(" ")
+    # reverse the words list
+    words = words[::-1]
+    # return as a string
+    return " ".join(words)
+
 
 def main():
     sys.stdin = open("strings/input.txt", "r")
     sys.stdout = open("strings/output.txt", "w")
-    input = sys.stdin.readline  
+    input = sys.stdin.readline
 
     # start here
     # s,t = list(map(str,input().split()))
-    string=input()
+    string = input()
 
-    print(sumBeautyBrute(string))
+    print(revWordsString(string))
+
+
 threading.Thread(target=main).start()
